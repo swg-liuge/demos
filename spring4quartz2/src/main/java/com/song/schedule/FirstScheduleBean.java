029aa94fdcbe5a612b5cda47609e77ac48598fe8
songweiguang <songweiguang@didichuxing.com>
2017-12-08 11:34:28 +0800

    test

--- spring4quartz2/src/main/java/com/song/schedule/FirstScheduleBean.java
+++ spring4quartz2/src/main/java/com/song/schedule/FirstScheduleBean.java
@@ -2,8 +2,10 @@ package com.song.schedule;
 
 import org.springframework.stereotype.Component;
 
+import java.io.Serializable;
+
 @Component
-public class FirstScheduleBean {
+public class FirstScheduleBean  implements Serializable{
 
     public void print(){
         System.out.println("hello world");
